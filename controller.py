8cd29f08f131b09e01e2696e59cd99215c2b91f4
Matthias Frei <mf@frei.media>
2022-03-06 19:28:00 +0100

    revert cleanup

--- controller.py
+++ controller.py
@@ -1129,6 +1129,53 @@ class SerialClient:
 
 class MqttClient:
 
+    class AsyncioHelper:
+        def __init__(self, loop, client):
+            self.log = logging.getLogger("mqtt-asyncio-helper")
+            self.loop = loop
+            self.client = client
+            self.client.on_socket_open = self.on_socket_open
+            self.client.on_socket_close = self.on_socket_close
+            self.client.on_socket_register_write = self.on_socket_register_write
+            self.client.on_socket_unregister_write = self.on_socket_unregister_write
+
+        def on_socket_open(self, client, userdata, sock):
+            self.log.debug("Socket opened")
+
+            def cb():
+                #self.log.debug("Socket is readable, calling loop_read")
+                client.loop_read()
+
+            self.loop.add_reader(sock, cb)
+            self.misc = self.loop.create_task(self.misc_loop())
+
+        def on_socket_close(self, client, userdata, sock):
+            self.log.debug("Socket closed")
+            self.loop.remove_reader(sock)
+            self.misc.cancel()
+
+        def on_socket_register_write(self, client, userdata, sock):
+            self.log.debug("Watching socket for writability.")
+
+            def cb():
+                self.log.debug("Socket is writable, calling loop_write")
+                client.loop_write()
+
+            self.loop.add_writer(sock, cb)
+
+        def on_socket_unregister_write(self, client, userdata, sock):
+            self.log.debug("Stop watching socket for writability.")
+            self.loop.remove_writer(sock)
+
+        async def misc_loop(self):
+            self.log.debug("misc_loop started")
+            while self.client.loop_misc() == mqtt_client.MQTT_ERR_SUCCESS:
+                try:
+                    await asyncio.sleep(1)
+                except asyncio.CancelledError:
+                    break
+            self.log.debug("misc_loop finished")
+
     def __init__(self, loop, config, on_connect_callback, on_disconnect_callback):
         self.log = logging.getLogger("mqtt")
 
@@ -1152,6 +1199,8 @@ class MqttClient:
         self.client.reconnect_delay_set(min_delay=1, max_delay=120)
         self.client.on_connect = self.on_connect
         self.client.on_disconnect = self.on_disconnect
+
+        self._aioh = self.AsyncioHelper(self.loop, self.client)
 
         # Try to connect to mqtt broker
         while True:
